0f7424d8b2c401ef71488f408bdea5303be99251
FuwaFuwa0204 <soyeonmail24@gmail.com>
2023-04-25 16:32:44 +0900

    add keyboardOptions,keyboardActions, Switch composable

--- app/src/main/java/com/fuwafuwa/tip_calculator/MainActivity.kt
+++ app/src/main/java/com/fuwafuwa/tip_calculator/MainActivity.kt
@@ -3,17 +3,20 @@ package com.fuwafuwa.tip_calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -45,7 +48,12 @@ fun TipTimeScreen() {
     //null이면 0.0
     val amount = amountInput.toDoubleOrNull() ?: 0.0
     //tip계산
-    val tip = calculateTip(amount)
+    var tipInput by remember { mutableStateOf("") }
+    val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
+    var roundUp by remember { mutableStateOf(false) }
+    val tip = calculateTip(amount,tipPercent,roundUp)
+    // Compose에서 포커스를 제어하는 데 사용. 이 변수를 사용하여 텍스트 상자로 포커스를 이동하고 텍스트 상자에서 포커스를 지운다.
+    val focusManager = LocalFocusManager.current
 
     Column(modifier=Modifier.padding(32.dp),
         //하위 요소 사이에 고정된 8dp 공백이 추가.
@@ -57,8 +65,31 @@ fun TipTimeScreen() {
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        EditNumberField(amountInput,{amountInput = it})
-
+        EditNumberField(
+            label = R.string.bill_amount,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext={focusManager.moveFocus(FocusDirection.Down)}
+            ),
+            value = amountInput,
+            onValueChange = { amountInput = it }
+        )
+        EditNumberField(
+            label = R.string.how_was_the_service,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {focusManager.clearFocus()}
+            ),
+            value = tipInput,
+            onValueChange = { tipInput=it }
+        )
+        RoundTheTipRow(roundUp = roundUp, onRoundUpChanged = { roundUp = it })
         Spacer(Modifier.height(24.dp))
 
         Text(
@@ -73,27 +104,57 @@ fun TipTimeScreen() {
 }
 
 @Composable
-fun EditNumberField(value:String, onValueChange:(String)->Unit) {
+//textfield가 여러개이기때문에 라벨도 인수로!
+fun EditNumberField(@StringRes label:Int,
+                    keyboardOptions: KeyboardOptions,
+                    keyboardActions: KeyboardActions,
+                    value:String,
+                    onValueChange:(String)->Unit,
+                    modifier:Modifier=Modifier) {
     //앱에 텍스트 입력
     TextField(
         value = value,
         onValueChange = onValueChange,
-        label = {Text(stringResource(id = R.string.cost_of_service))},
+        label = {Text(stringResource(id =label))},
         modifier = Modifier.fillMaxWidth(),
         singleLine = true,
         //화면에 표시되는 키보드를 구성하여 숫자, 이메일 주소, URL, 비밀번호 등을 입력할 수 있는 옵션이 있다.
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions
 
     )
 }
 
-//퍼센트는 하드코딩
-private fun calculateTip(amount:Double,tipPercent:Double = 15.0): String{
+private fun calculateTip(amount:Double,tipPercent:Double = 15.0, roundUp: Boolean): String{
 
-    val tip = tipPercent / 100 * amount
+    var tip = tipPercent / 100 * amount
+    //화폐단위 표시
+    if(roundUp){
+        //반올림
+        tip = kotlin.math.ceil(tip)
+    }
     return NumberFormat.getCurrencyInstance().format(tip)
 
+}
+
+@Composable
+fun RoundTheTipRow(roundUp: Boolean,
+                   onRoundUpChanged: (Boolean) -> Unit,
+                   modifier: Modifier = Modifier) {
+    Row(modifier= modifier
+        .fillMaxWidth()
+        .size(48.dp), verticalAlignment = Alignment.CenterVertically){
+        Text(text = stringResource(R.string.round_up_tip))
+        Switch(
+            modifier= modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End),
+            checked = roundUp,
+            onCheckedChange = onRoundUpChanged,
+            colors = SwitchDefaults.colors(uncheckedThumbColor = Color.DarkGray)
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
